a32b08dd636aa8f89495474cf40573b4c5d0e23c
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-14 02:05:05 +0300

    added task 01

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int a=0,b=0,c=0; //целые
+float f=20.4f; //дробные
+double d=3.14; //вещественные
+string s="asdasd";
+char c0='a';
+bool flag=true;
+int переменная1=0;
+*/
+
+//1. С клавиатуры вводится целое число. Вывести квадрат числа (блок-схема)
+
+Console.Clear();
+
+int a;
+System.Console.Write("Введи число: ");
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+int b=a*a;
+System.Console.WriteLine(a+"^2="+b);
+System.Console.WriteLine($"{a}^2={b}"); 
