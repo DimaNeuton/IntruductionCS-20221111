5922fb7e577edfce64717c4a50be7151f23f14d0
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-19 16:31:50 +0300

    1 corrected task 12

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,7 +1,9 @@
 ﻿// 12.	С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
 
+/*
 System.Console.Write("Введите число от 10 до 99: ");
 int a=Convert.ToInt32(Console.ReadLine());
+
 
 if (a%10>a/10)
 {
@@ -11,3 +13,21 @@ else
 {
     System.Console.Write($"{a/10} - наибольшая цифра");
 }
+*/
+
+ int a;
+ do
+ {
+    System.Console.Write("Введите число от 10 до 99: ");
+    a=Convert.ToInt32(Console.ReadLine());
+     if (a<10 || a>99)
+    {
+    System.Console.WriteLine("Введено неверное число");
+    }
+ }
+ while (a<10 || a>99);
+
+int max;
+ if (a%10>a/10) max=a%10; else max=a/10;
+    System.Console.Write($"{max} - наибольшая цифра");
+    
