3b61dffd73c7dc263042fa62b1827d5d13b178a1
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-20 17:53:09 +0300

    added task 18

---
+++ 18/Program.cs
@@ -0,0 +1,7 @@
+﻿// 18.	По двум заданным числам проверять является ли одно квадратом другого
+
+System.Console.Write("Ведите a: ");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Ведите b: ");
+double b=Convert.ToDouble(Console.ReadLine());
+if (a==b*b || b==a*a) System.Console.WriteLine("Один является квадратом другого"); else System.Console.WriteLine("No");
