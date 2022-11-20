e3354f19037f90f9ff38a417b7e11d0021f4c17c
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-20 20:16:16 +0300

    added task 22

---
+++ 22/Program.cs
@@ -0,0 +1,6 @@
+﻿// 22.	Программа проверяет пятизначное число на палиндромом
+
+System.Console.Write("Введи пятизначное число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+if (n/10000==n%10 && (n-n/10000*10000)/1000==(n-n/100*100)/10) System.Console.WriteLine($"{n} - Палиндром");
+else System.Console.WriteLine("Попробуй еще");
