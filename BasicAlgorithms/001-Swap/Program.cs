ffae8398374c3141a19417351f04dded4347d0d8
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-20 15:54:47 +0300

    added 002-Счетчик и флаг

--- BasicAlgorithms/001-Swap/Program.cs
+++ BasicAlgorithms/001-Swap/Program.cs
@@ -16,26 +16,9 @@ a=a+b;
 b=a-b;
 a=a-b;*/
 
-/*
 int a=5;
 int b=7;
 a=a^b;
 b=a^b;
 a=a^b;
 System.Console.WriteLine($"a={a} b={b}");
-*/
-
-System.Console.WriteLine("Введите количество учеников: ");
-int N=Convert.ToInt32(Console.ReadLine());
-int bad_ans_count=0;
-int ans10_count=0;
-for (int i=1;i<=N;i++)
-{
-    System.Console.WriteLine($"Введите количество решенных примеров {i} ученика: ");
-    int a=Convert.ToInt32(Console.ReadLine());
-    if (a<5) bad_ans_count++;
-    if (a==10) ans10_count=1;
-}
-if (ans10_count==1) System.Console.WriteLine("Yes");
-else System.Console.WriteLine("No");
-System.Console.WriteLine($"Количество неудовлетворительных оценок - {bad_ans_count}");
---
+++ BasicAlgorithms/002-Счетчик и флаг/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+System.Console.WriteLine("Введите количество учеников: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int k=0;
+bool flag=false;   // флаг
+for (int i=1;i<=N;i++)
+{
+    System.Console.WriteLine($"Введите количество решенных примеров {i} ученика: ");
+    int a=Convert.ToInt32(Console.ReadLine());
+    if (a<5) k++; 
+    if (a==10) flag=true;
+}
+if (flag) System.Console.WriteLine("Yes");
+else System.Console.WriteLine("No");
+System.Console.WriteLine($"Количество неудовлетворительных оценок - {k}");
