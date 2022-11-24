a8c1d450c7b8f847b7bb9eadfa9e8a87da526bb1
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-24 18:37:21 +0300

    added task 31

---
+++ 31/Program.cs
@@ -0,0 +1,19 @@
+﻿// 31. Сгенерировать 10 слкчайных чисел и показать кубы чисел, заканчивающихся на четную цифру
+
+/*
+int r=10;
+double t=r; //неявно приведение типов
+double x=23;
+//int 0=Convert.ToInt32(x);
+int o=(int)x; //явное приведение типов
+*/
+
+Random rando=new Random(10);
+for (int i=0;i<5;i++)
+{
+    int a=rando.Next(1,100);
+    int b=a; //(int)Math.Pow(a,3);
+    if (b%2==0)
+
+    System.Console.WriteLine($"{a}, {b}");
+}
