5d9e92f6c44803e77b446859f868b6843e4c5833
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-25 19:48:13 +0300

    really added Task 24

---
+++ 24/Program.cs
@@ -0,0 +1,11 @@
+﻿// 24.	Вывести на экран таблицу квадратов чисел от 1 до N
+
+int Kvadrat(int i)
+{
+    int k=(int)Math.Pow(i,2);
+    return k;
+}
+System.Console.WriteLine("Write N");
+int N=Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=N; i++)
+    System.Console.WriteLine(Kvadrat(i));
