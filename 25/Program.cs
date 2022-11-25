0e8f72a3fac693216fc0ab7ffe788907dbdb0690
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-25 19:52:23 +0300

    added Task 25

---
+++ 25/Program.cs
@@ -0,0 +1,11 @@
+﻿// 25.	Вывести на экран кубы чисел от 1 до N
+
+int Kub(int i)
+{
+    int k=(int)Math.Pow(i,3);
+    return k;
+}
+System.Console.WriteLine("Write N");
+int N=Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=N; i++)
+    System.Console.WriteLine(Kub(i));
