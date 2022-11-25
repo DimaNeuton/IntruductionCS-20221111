e140e8d8006a4ff0ecc61d49e3c9bc4f9fc770c3
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-25 19:02:40 +0300

    added 004-EUQLID

---
+++ BasicAlgorithms/004-EUQLID/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Алгоритм Евклида
+
+int k=0;
+int GCD(int a, int b)
+{
+    while (a!=b)
+    {
+        k++;
+                if (a>b)
+            a=a-b;
+        else
+            b=b-a;
+    }
+    return a;
+}
+System.Console.WriteLine(GCD(15, 20));
+System.Console.WriteLine(k);
+*/
+
+// Ускоренный Алгоритм Евклида
+
+int k=0;
+int QuickGCD(int a, int b)
+{
+    while (a!=0 && b!=0)
+    {
+        k++;
+                if (a>b)
+            a=a%b;  
+        else
+            b=b%a;
+    }
+    return a+b;
+}
