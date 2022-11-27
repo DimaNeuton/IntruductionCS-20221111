202da3252ce547959b9b8c1eb0c42ca798853987
Dmitriy <gavrilov_d_s@mail.ru>
2022-11-27 08:35:06 +0300

    added Tasks 29, 30

---
+++ 29/Program.cs
@@ -0,0 +1,17 @@
+﻿// 29.	Подсчитать сумму цифр в числе
+
+int Numb(int a)
+{
+    int b = a/10;
+    return b;
+}
+
+System.Console.Write("Write number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (number>0)
+{
+    number = Numb(number);
+    i++;
+}
+System.Console.WriteLine($"Number of digits in this number- {i}");
---
+++ 30/Program.cs
@@ -0,0 +1,18 @@
+﻿// 30.	Написать программу вычисления произведения чисел от 1 до N
+
+int Fact(int n, int i)
+{
+    n = n * (i + 1);
+    return n;
+}
+
+System.Console.Write("Write N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+int i = 1;
+while (i < N)
+{
+    n = Fact(n, i);
+    i++;
+}
+System.Console.WriteLine($"Factorial of {N} = {n}");
